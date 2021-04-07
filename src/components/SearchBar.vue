12d2007c16bebafe4acb7b00e1e55d837e473e05
Ethan Franson <ethan.r.franson@gmail.com>
2021-04-06 18:05:27 -0600

    first commit to this repo

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,26 @@
+<template>
+<label class="searchBar__label" for="searchBar__input">
+    <div class="searchBar">
+        
+            <img class="searchBar__icon" src="../assets/search.svg" alt="">
+            <input class="searchBar__input" id="searchBar__input" v-model="searchValue" type="text" placeholder="Try searching poodles...">
+        
+    </div>
+</label>
+    
+</template>
+
+<script>
+export default {
+    name: "SearchBar",
+    data(){
+        return {
+            searchValue: "",
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+    @import '../sass/components/_searchBar.scss';
+</style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class=homePage>
+        <nav-home></nav-home>
+        <div class=test></div>
+    </div>
+</template>
+
+<script>
+import NavHome from '../components/NavHome';
+export default {
+    name: 'HomePage',
+    components: {
+        NavHome
+    }
+}
+</script>
+
+<style lang="scss">
+    @import '../sass/pages/_homePage.scss';
+    .test{
+        height: 100px;
+    }
+</style>
